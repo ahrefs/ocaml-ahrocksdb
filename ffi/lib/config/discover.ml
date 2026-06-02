b970fd69f77a98e9e4f9eb0d3f5b4b8e9df9433d
ip <igor@ahrefs.com>
2026-06-02 12:21:57 -0400

    fix stubgen.sh

--- ffi/lib/config/discover.ml
+++ ffi/lib/config/discover.ml
@@ -1,30 +1,16 @@
-open Printf
+open Configurator.V1
 
-let minimum_rocks_major, minimum_rocks_minor = 5, 14
-
-module C = Configurator.V1
-
-let () = C.main ~name:"librocksdb" begin fun c ->
-
-let c_flags, link_flags =
-  (* need system cflags otherwise `<rocks/c.h>` can't be `#include`d *)
-  Unix.putenv "PKG_CONFIG_ALLOW_SYSTEM_CFLAGS" "1";
-  match C.Pkg_config.get c with
-  | None ->
-    eprintf "discover requires pkg-config\n";
-    C.die "discover error"
-  | Some pc ->
-      let expr = sprintf "rocksdb >= %d.%d" minimum_rocks_major minimum_rocks_minor in
-      match C.Pkg_config.query_expr_err pc ~package:"rocksdb" ~expr with
-      | Error s ->
-          eprintf "pkg-config query error: %s\n" s;
-          C.die "discover error"
-      | Ok { C.Pkg_config.cflags; libs} -> cflags, libs
-in
-
-C.Flags.write_sexp  "c_flags.sexp"          c_flags;
-C.Flags.write_sexp  "c_library_flags.sexp"  link_flags;
-C.Flags.write_lines "c_flags.txt"           c_flags;
-C.Flags.write_lines "c_library_flags.txt"   link_flags;
-
+let () = main ~name:"librocksdb" begin fun c ->
+  let c_flags, link_flags =
+    match Pkg_config.get c with
+    | None -> die "discover requires pkg-config"
+    | Some pc ->
+    match Pkg_config.query_expr_err pc ~package:"rocksdb" ~expr:"rocksdb >= 5.14" with
+      | Error s -> die "pkg-config query error: %s" s;
+      | Ok { Pkg_config.cflags; libs} -> cflags, libs
+  in
+  Flags.write_sexp  "c_flags.sexp"          c_flags;
+  Flags.write_sexp  "c_library_flags.sexp"  link_flags;
+  Flags.write_lines "c_flags.txt"           c_flags;
+  Flags.write_lines "c_library_flags.txt"   link_flags;
 end
