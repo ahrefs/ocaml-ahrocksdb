f324a760e360f7df03f356c9bafea6d1ccb7a8e5
Raphaël Proust <code@bnwr.net>
2026-06-19 21:04:26 +0200

    discover script for static linking

--- ffi/lib/config/discover.ml
+++ ffi/lib/config/discover.ml
@@ -1,7 +1,47 @@
 open Configurator.V1
 
+let pkg_config_raw ?(args=[]) ~package c =
+match Process.run c "pkg-config" (args @ [ package ]) with
+| { exit_code = 0; stdout; _ } -> String.trim stdout
+| { stderr; _ } -> die "cannot run pkg-config: %s\n" stderr
+
 let () = main ~name:"librocksdb" begin fun c ->
+
+(* upstream pc files are inaccurate, debian inherits this inaccuracy, we fix it *)
+let system_info = pkg_config_raw ~args:["--libs"; "--static"] ~package:"rocksdb" c in
+let () =
+  match system_info with
+  | "-lrocksdb" -> begin
+    (* innacurate pc file detected *)
+    let cwd = Sys.getcwd () in
+    let () = match Process.run c "cp" [pkg_config_raw ~args:["--path"] ~package:"rocksdb" c; Filename.concat cwd "rocksdb.pc"] with
+      | { exit_code = 0; _ } -> ()
+      | _ -> die "cannot cp pc file"
+    in
+    let libs_private =
+      let std_lib_dirs = [ "/usr/lib"; "/usr/lib/x86_64-linux-gnu"; "/usr/lib/aarch64-linux-gnu"; "/usr/local/lib"; "/usr/lib64"; ] in
+      let has_static_lib name =
+        List.exists
+          (fun dir -> Sys.file_exists (Filename.concat dir ("lib" ^ name ^ ".a")))
+          std_lib_dirs
+      in
+      List.filter_map (fun name -> if has_static_lib name then Some ("-l" ^ name) else None) ["z"; "snappy"; "lz4"; "bz2"; "zstd"]
+      @ ["-ldl"; "-lpthread"; "-lstdc++"]
+    in
+    let () = match Process.run c "sh" ["-c"; Printf.sprintf "echo 'Libs.private: %s' >> ./rocksdb.pc" (String.concat " " libs_private);] with
+      | { exit_code = 0; _ } -> ()
+      | _ -> die "cannot append to pc file"
+    in
+    let existing = match Sys.getenv_opt "PKG_CONFIG_PATH" with | Some s -> ":" ^ s | None -> "" in
+    let () = Unix.putenv "PKG_CONFIG_PATH" (cwd ^ existing) in
+    ()
+  end
+  | _ -> ()
+in
+
+
   let c_flags, link_flags =
+    let () = Unix.putenv "PKG_CONFIG_ARGN" "--static" in
     match Pkg_config.get c with
     | None -> die "discover requires pkg-config"
     | Some pc ->
